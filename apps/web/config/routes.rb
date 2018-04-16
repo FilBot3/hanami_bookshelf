fcd91a0c7197df5848de11a85c0b4fbeb1eb82fb
Phillip dudley <predatorian@localhost.localdomain>
2018-04-16 18:03:55 -0500

    Intial Commit - Documentation coming soon.

---
+++ apps/web/config/routes.rb
@@ -0,0 +1,11 @@
+# Configure your routes here
+# See: http://hanamirb.org/guides/routing/overview/
+#
+# Example:
+# get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
+
+root to: 'home#index'
+#post '/books',     to: 'books#create'
+#get  '/books/new', to: 'books#new'
+#get  '/books',     to: 'books#index'
+resources :books, only: [:index, :new, :create]
